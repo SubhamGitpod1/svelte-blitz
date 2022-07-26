a7a50566ef7a963c3af6ce21bb527e0e8dbef13a
SBHattarj <subhambhsubhambh@gmail.com>
2022-07-26 10:30:32 +0000

    first commit

--- start.sh
+++ start.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
+git submodule update --init
+
+cd vite-ts-path-with-multy-index-support
+npm i
+npm run build-lib
+cd ..
+
 npm i blitz@alpha -g
 npm i
 export DATABASE_URL="file:./db.sqlite"
